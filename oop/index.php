6a9492f1dfddf3ee232fd71584b1ea3d4a4ebd5b
Claire <c.riquier@gmail.com>
2015-03-25 12:40:57 +0100

    mise en place du site

---
+++ oop/index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>Starship invader</title>
+    <meta name="description" content="Toutes les planètes accessibles de notre système solaire">
+    <link rel="stylesheet" type="text/css" href="css/desktop.css" media="screen and (min-width:481px), projection">
+    <link rel="stylesheet" type="text/css" href="css/mobile.css" media="only screen and (min-width:0px) and (max-width:480px)">
+        <!--[if lt IE9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <div class="wrapper">
+        <header>
+            <h1 class="logo"><a href="index.html"><img src="img/logo.png" alt="logo"></a></h1>
+            <h2 class="title">Startship Invader</h2>
+        <nav class="nav">
+            <ul>
+                <li>
+                    <a href="index.php" class ="active">Accueil</a>
+                </li>
+                <li>
+                    <a href=""></a>
+                </li>
+                <li>
+                    <a href=""></a>
+                </li>
+                <li>
+                    <a href=""></a>
+                </li>
+            </ul>
+        </nav><!--fin nav-->
+        </header><!--fin header-->
+        <main>
+        </main><!--fin main-->
+        <footer>
+        </footer><!--fin footer-->
+    </div><!--fin wrapper-->
+</body>
+</html>
